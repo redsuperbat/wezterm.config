6955a648bb65c252684b4415f0bc11eb9b0ff658
Max Netterberg <max.netterberg@gmail.com>
2024-02-22 20:52:18 +0100

    update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,32 +14,32 @@ local keybind = function(key, mod, action)
 end
 
 local act = wezterm.action
-config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 }
+-- config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 }
 
-for i = 1, 9 do
-	keybind(tostring(i), "LEADER", act.ActivateTab(i - 1))
-end
-keybind("0", "LEADER", act.ActivateTab(-1))
-keybind("x", "LEADER", act.CloseCurrentPane({ confirm = false }))
-keybind("c", "LEADER", act.SpawnTab("CurrentPaneDomain"))
+-- for i = 1, 9 do
+-- keybind(tostring(i), "LEADER", act.ActivateTab(i - 1))
+-- end
+-- keybind("0", "LEADER", act.ActivateTab(-1))
+-- keybind("x", "LEADER", act.CloseCurrentPane({ confirm = false }))
+-- keybind("c", "LEADER", act.SpawnTab("CurrentPaneDomain"))
 
 -- Eu keyboards
 keybind("(", "ALT|SHIFT", act.SendString("{"))
 keybind(")", "ALT|SHIFT", act.SendString("}"))
 keybind("/", "ALT|SHIFT", act.SendString("\\"))
 -- Pane Management
-keybind("%", "LEADER", act.SplitHorizontal({ domain = "CurrentPaneDomain" }))
-keybind('"', "LEADER", act.SplitVertical({ domain = "CurrentPaneDomain" }))
+-- keybind("%", "LEADER", act.SplitHorizontal({ domain = "CurrentPaneDomain" }))
+-- keybind('"', "LEADER", act.SplitVertical({ domain = "CurrentPaneDomain" }))
 
-keybind("h", "CTRL", act.ActivatePaneDirection("Left"))
-keybind("j", "CTRL", act.ActivatePaneDirection("Down"))
-keybind("k", "CTRL", act.ActivatePaneDirection("Up"))
-keybind("l", "CTRL", act.ActivatePaneDirection("Right"))
+-- keybind("h", "LEADER", act.ActivatePaneDirection("Left"))
+-- keybind("j", "LEADER", act.ActivatePaneDirection("Down"))
+-- keybind("k", "LEADER", act.ActivatePaneDirection("Up"))
+-- keybind("l", "LEADER", act.ActivatePaneDirection("Right"))
 
 keybind("v", "LEADER", act.ActivateCopyMode)
-keybind("z", "LEADER", act.TogglePaneZoomState)
+-- keybind("z", "LEADER", act.TogglePaneZoomState)
 -- View Management
-keybind("Enter", "ALT", act.ToggleFullScreen)
+-- keybind("Enter", "ALT", act.ToggleFullScreen)
 keybind("-", "CMD", act.DecreaseFontSize)
 keybind("+", "CMD", act.IncreaseFontSize)
 keybind("0", "CMD", act.ResetFontSize)
